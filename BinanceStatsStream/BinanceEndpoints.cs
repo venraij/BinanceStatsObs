adfd2259fe9d91d835f79a7be43fb5d28fc0210f
Nick van Raaij <S1149241@student.windesheim.nl>
2021-02-11 22:36:02 +0100

    Added some comments

--- BinanceStatsStream/BinanceEndpoints.cs
+++ BinanceStatsStream/BinanceEndpoints.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.IO;
 using Newtonsoft.Json.Linq;
 
@@ -13,21 +8,24 @@ namespace View
 {
     public static class BinanceEndpoints
     {
-        private static string apiUri = "https://fapi.binance.com";
+        private static string apiUri = "https://fapi.binance.com"; // Futures API base url of Binance
         private static string recvWindow = "5000";
 
+        // The binance API key
         public static string apiKey
         {
             get;
             set;
         }
 
+        // The Binance API secret
         public static string secretKey
         {
             get;
             set;
         }
 
+        // Grabs all the the futures account info of the account associated with the Api key and Secret key
         public static void FuturesAccountInformation()
         {
             string uri = apiUri + "/fapi/v2/account?";
@@ -36,17 +34,19 @@ namespace View
             string parameters = $"recvWindow={recvWindow}&timestamp={timestamp}";
             string signature = HashEncode(HashHMAC(StringEncode(secretKey), StringEncode(parameters)));
 
+            // Create the full URI for the request
             uri = $"{uri + parameters}&signature={signature}";
 
-            //var jsonObj = JsonSerializer.Serialize(Request.Get(uri, apiKey), new JsonSerializerOptions{ WriteIndented = true });
             dynamic response = JObject.Parse(Request.Get(uri, apiKey));
 
             DirectoryCheck();
 
+            // Write the received data to sepereate TXT files
             File.WriteAllText(@"C:\BinanceStats\Total_PNL.txt", Convert.ToString(response.totalCrossUnPnl));
             File.WriteAllText(@"C:\BinanceStats\Total_Balance.txt", Convert.ToString(response.totalWalletBalance));
         }
 
+        // Check is if the BinanceStats directory exists
         public static void DirectoryCheck()
         {
             if (!System.IO.Directory.Exists(@"C:\BinanceStats\"))
@@ -55,26 +55,24 @@ namespace View
             }
         }
 
+        // Hashes the specifed key and message in HMAC SHA256
         private static byte[] HashHMAC(byte[] key, byte[] message)
         {           
             var hash = new HMACSHA256(key);
             return hash.ComputeHash(message);
         }
 
+        // Encodes a string to ASCII
         private static byte[] StringEncode(string text)
         {
             var encoding = new ASCIIEncoding();
             return encoding.GetBytes(text);
         }
 
+        // Encodes a hash to a string
         private static string HashEncode(byte[] hash)
         {
             return BitConverter.ToString(hash).Replace("-", "").ToLower();
-        }
-
-        private static void writeToFile()
-        {
-
         }
     }
 }
