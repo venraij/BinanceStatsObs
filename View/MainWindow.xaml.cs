202097967194b9b8134ba9af58a20900ebfa54f3
Nick van Raaij <S1149241@student.windesheim.nl>
2021-02-12 20:15:47 +0100

    Removed some deprecated stuff

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using View;
+using Controller;
 
 namespace BinanceStatsStream
 {
